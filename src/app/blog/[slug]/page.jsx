c48794cfe47748288f072ee20a9d7656667222b3
yang <allanustw@gmail.com>
2024-02-26 20:41:29 +0800

    blog page design

---
+++ src/app/blog/[slug]/page.jsx
@@ -0,0 +1,9 @@
+const BlogPage = async () => { 
+  return (
+    <div>
+      <h1>blogpage</h1>
+      
+    </div>
+  );
+}
+export default BlogPage;
---
+++ src/app/blog/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from "./blog.module.css";
+import PostCard from "@/components/postCard/postCard";
+
+const BlogPage = async () => { 
+  return (
+    <div className={styles.container}>
+        <div className={ styles.post}><PostCard/></div>
+        <div className={ styles.post}><PostCard/></div>
+        <div className={ styles.post}><PostCard/></div>
+        <div className={ styles.post}><PostCard/></div>
+    </div>
+  );
+}
+export default BlogPage;
