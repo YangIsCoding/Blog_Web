4b089acbba01b17fd9d7ce5ccc3aaa0df28783c4
yang <allanustw@gmail.com>
2024-02-27 19:32:11 +0800

    self intro

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -31,7 +31,8 @@ const AboutPage = () => {
 
 This realization dawned on me during a philosophical debate on materialism versus idealism. I came to understand that the internet and server concepts are perfect metaphors for idealism. Our world is replete with virtual constructs like currency, nationality, and law - all born from collective human consensus and shaping our social fabric. These days, such constructs predominantly reside in the cloud, significantly influencing sectors like healthcare, finance, and transportation. This transition to the virtual underscores a vital truth about our society&apos;s structure. So, enhancing these virtual elements is crucial for world improvement, with computer science playing a pivotal role.
 
-        </p>
+          </p>
+          
         <div className={styles.boxes}>
           <div className={styles.box}>
             <h1>100000 +</h1>
