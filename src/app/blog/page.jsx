482b4ad67df19a2e3dc5f1acd20c4ff4dfff82c3
yang <allanustw@gmail.com>
2024-02-27 10:44:42 +0800

    0227 10:44

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,15 +1,45 @@
-import React from 'react';
-import styles from "./blog.module.css";
+"use client"
+import React from "react";
+import { useState } from "react";
+
 import PostCard from "@/components/postCard/postCard";
+import styles from "./blog.module.css";
+
+
+const getData = async () => { 
+  
+  const host = process.env.HOST
+  const url = "http://localhost:3000/api/posts"
+  const url2 = `${host}/api/posts`
+  console.log("URL:", url2);
+  const res = await fetch(url2, { 
+    method: 'GET', 
+    cache: 'no-store' 
+  });
+
+  if (!res.ok) { 
+    throw new Error("Failed to fetch data");
+  }
+  return res.json(); 
+};
+
+
+
 
 const BlogPage = async () => { 
+  
+  const posts = await getData();
   return (
     <div className={styles.container}>
-        <div className={ styles.post}><PostCard/></div>
-        <div className={ styles.post}><PostCard/></div>
-        <div className={ styles.post}><PostCard/></div>
-        <div className={ styles.post}><PostCard/></div>
-    </div>
-  );
+      {
+        posts.map((post) => (
+          <div className={styles.post} key={post.id}>
+            <PostCard post={post} />
+          </div>
+        ))
+      }
+      
+    </div>);
 }
+
 export default BlogPage;
--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -25,3 +25,34 @@ const PostCard = ({ }) => {
 }
 
 export default PostCard
+
+/*import Image from "next/image"
+import styles from "./postCard.module.css"
+import Link from "next/link"
+
+const PostCard = ({ post }) => {
+  //console.log(post);
+  return (
+    <div className={styles.container}>
+
+      <div className={styles.top}>
+        {
+          <div className={styles.imgContainer}>
+            <Image src={post.postImg} alt="" fill className={styles.img}/>
+          </div>
+        }
+        <span className={styles.date}>{post.postTime}</span>
+      </div>
+
+      <div className={styles.bottom}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <p className={styles.desc}>
+          {post.shortdesc.length > 25 ? post.shortdesc.substring(0, 25) + "..." : post.shortdesc}
+        </p>
+        <Link className={styles.link} href={`/blog/${post.id}`}>READ MORE</Link>
+      </div>
+    </div>
+  )
+}
+
+export default PostCard*/
