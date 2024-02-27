8728f3f35c517f0c6a7973f3c0f35e8a5e054223
yang <allanustw@gmail.com>
2024-02-27 19:34:09 +0800

    self intro

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -29,7 +29,7 @@ const AboutPage = () => {
         <p className={styles.desc}>
           &quot;The world is a simulation.&quot;<br></br>
 
-This realization dawned on me during a philosophical debate on materialism versus idealism. I came to understand that the internet and server concepts are perfect metaphors for idealism. Our world is replete with virtual constructs like currency, nationality, and law - all born from collective human consensus and shaping our social fabric. These days, such constructs predominantly reside in the cloud, significantly influencing sectors like healthcare, finance, and transportation. This transition to the virtual underscores a vital truth about our society&apos;s structure. So, enhancing these virtual elements is crucial for world improvement, with computer science playing a pivotal role.
+This realization dawned on me during a philosophical debate on materialism versus idealism. I came to understand that the internet and server concepts are perfect metaphors for idealism. Our world is replete with virtual constructs like currency, nationality, and law - all born from collective human consensus and shaping our social fabric. These days, such constructs predominantly reside in the cloud, significantly influencing sectors like healthcare, finance, and transportation. This transition to the virtual underscores a vital truth about our society&#39;s structure. So, enhancing these virtual elements is crucial for world improvement, with computer science playing a pivotal role.
 
           </p>
           
