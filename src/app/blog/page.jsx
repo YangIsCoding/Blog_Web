de3de9e9510be03c1da68842a5ef23012cfc102b
Yang <pin-yang.chen@duke.edu>
2024-08-22 16:20:11 -0400

    mac

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -32,6 +32,16 @@ const BlogPage = () => {
 
   return (
     <>
+    <head>
+        <title>P.Y,Chen - Blog</title>
+        <meta name="description" content="Chen Pin Yang's personal portfolio, showcasing skills, experience, and projects in Fintech and software development." />
+        <meta property="og:title" content="P.Y,Chen - Home" />
+        <meta property="og:description" content="Chen Pin Yang's personal portfolio, showcasing skills, experience, and projects in Fintech and software development." />
+        {/* 确保 og:image 的路径是静态且可访问的 */}
+        <meta property="og:image" content="https://www.chenpinyangdev.com/profile_yang.jpg" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.chenpinyangdev.com/" />
+      </head>
 
     <div className={styles.container}>
       {posts.map((post) => (
